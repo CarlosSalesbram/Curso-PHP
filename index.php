384086cab09b005af99559e3b0641eb75e1b5a30
Carlos <salesbramcarlos@gmail.com>
2025-07-25 09:53:12 -0300

    atualizando estudos

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
-echo"Hello world!"; 
+
+function chamaNome(){
+    
+    $nome = "Carlos";
+
+    $objContato = new Contato;
+
+    $objContato->setNome($nome);
+
+    $contatoNome = $objContato->getNome();
+
+    echo "$contatoNome";
+}
+
+class Contato {
+    private $nome;
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+}
+
+chamaNome();
